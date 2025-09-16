87adbcb1f4514ac579c5825dafb341a50fb32baf
z7935 <168332217+z7935@users.noreply.github.com>
2025-09-17 00:57:47 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    Q_INIT_RESOURCE(images);
+
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
+}
